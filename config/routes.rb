430fe33c0d0caf7f53b82b9b16cefd37e34cc237
Lisa Conti <leconti2@gmail.com>
2018-11-23 15:56:27 +1100

    fixed cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   authenticated :user do
   	root :to => 'pages#dashboard', as: :authenticated
 	end
@@ -8,11 +8,10 @@ Rails.application.routes.draw do
 	root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products, only: [:show, :index] do
-    resources :order_items, only: [:create]
     resources :quick_buy_items, only: [:create]
   end
 
-  resources :order_items, only: [:edit, :update, :destroy]
+  resources :order_items, only: [:create, :edit, :update, :destroy]
   resources :orders, only: [:show]
   resources :quick_buy_items, only: [:edit, :update, :destory]
 
