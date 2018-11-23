44f6c61dd0dcb86b561bb868fe2358cfb9179c8c
Jessie <jessie.baxter29@gmail.com>
2018-11-24 05:24:21 +1100

    Added dependent destroy to product and variants

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,8 @@
 class Product < ApplicationRecord
 	has_many :quick_buy_items
-  has_many :varients
+  has_many :varients, dependent: :destroy
   has_many :inventories, through: :varients
 
 	validates :name, presence: true
 	validates :brand, presence: true
-
 end
