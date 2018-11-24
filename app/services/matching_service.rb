af7a3218f1c7ed4e6747b2dfa23a0e91eaf76188
Jessie <jessie.baxter29@gmail.com>
2018-11-24 17:45:36 +1100

    Fixed bug on matching service

--- app/services/matching_service.rb
+++ app/services/matching_service.rb
@@ -1,18 +1,19 @@
-def MatchingService
+class MatchingService
 
 	def initialize
 	end
 
 	def product_found(product)
 		puts "Checking for product..."
-		return Product.where("brand ILIKE ? AND name ILIKE ?", "%#{product[:brand]}%", "%#{product[:name]}%").nil?
+		return Product.where("brand ILIKE ? AND name ILIKE ?", "%#{product[:brand]}%", "%#{product[:name]}%").present?
 	end
 
 	def variant_found(variant, product_id)
 		puts "Checking for variant..."
-		return Varient.where("name ILIKE ? AND product_id = ?", "%#{variant[:name]}%", product_id).nil?
+		return Varient.where("name ILIKE ? AND product_id = ?", "%#{variant[:name]}%", product_id).present?
 	end
 end
+
 
 
 
