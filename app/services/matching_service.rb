d57bdf52b1e9ff3f59ab27027561a7325ea26c2f
Jessie <jessie.baxter29@gmail.com>
2018-11-24 15:30:18 +1100

    Updated matching service syntax

--- app/services/matching_service.rb
+++ app/services/matching_service.rb
@@ -3,13 +3,13 @@ def MatchingService
 	def initialize
 	end
 
-	def product_not_found?(product)
-		puts "Checking if product is found..."
+	def product_found(product)
+		puts "Checking for product..."
 		return Product.where("brand ILIKE ? AND name ILIKE ?", "%#{product[:brand]}%", "%#{product[:name]}%").nil?
 	end
 
-	def variant_not_found?(variant, product_id)
-		puts "Checking if variant is found..."
+	def variant_found(variant, product_id)
+		puts "Checking for variant..."
 		return Varient.where("name ILIKE ? AND product_id = ?", "%#{variant[:name]}%", product_id).nil?
 	end
 end
