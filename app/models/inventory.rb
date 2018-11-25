7672a8ad332e75d42750ac32ac11ce7e9a66b01a
Lisa Conti <leconti2@gmail.com>
2018-11-25 11:40:56 +1100

    few tweaks

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,7 +1,7 @@
 class Inventory < ApplicationRecord
   belongs_to :varient
   belongs_to :seller
-
+  has_many :shippings, through: :seller
   monetize :price_cents
 
   validates :source_url, presence: true, uniqueness: true
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,3 +1,4 @@
 class Shipping < ApplicationRecord
   belongs_to :seller
+  monetize :cost_cents
 end
