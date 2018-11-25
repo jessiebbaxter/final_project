ca2da883d861164eb710eb1448ae8d61f0bbaa0e
Jessie <jessie.baxter29@gmail.com>
2018-11-25 13:26:00 +1100

    v1 of quick buy list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,19 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+  	quick_buy_list
   end
 
+  def quick_buy_list
+  	orders = Order.where(["user_id = ? and state = ?", current_user.id, "paid"])
+ 		@quick_buy_list = []
+ 		orders.each do |order|
+ 			order.inventories.each do |inventory|
+ 				@quick_buy_list << {
+ 					product: inventory.varient.product,
+ 					inventory: inventory
+ 				}
+ 			end
+ 		end
+ 	end
 end
