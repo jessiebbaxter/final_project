892d3e4db8d5cf0bcd030111fa8a1582ad0e3f4c
Lisa Conti <leconti2@gmail.com>
2018-11-25 14:08:36 +1100

    one-click is working but we need to move it to the dashboard

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,21 +6,40 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    customer = Stripe::Customer.create(
-      source: params[:stripeToken],
-      email:  params[:stripeEmail]
-    )
+    if current_user.customer_id
+      one_click_purchases
+    else
+      customer = Stripe::Customer.create(
+        source: params[:stripeToken],
+        email:  params[:stripeEmail]
+      )
 
+      charge = Stripe::Charge.create(
+        customer:     customer.id,
+        amount:       @order.amount_cents,
+        description:  "Payment for order #{@order.id}",
+        currency:     @order.amount.currency
+      )
+
+      current_user.update(customer_id: customer.id)
+      @order.update(payment: charge.to_json, state: 'paid')
+      redirect_to orders_complete_path(@order)
+    end
+    rescue Stripe::CardError => e
+      flash[:alert] = e.message
+      redirect_to new_order_payment_path(@order)
+  end
+
+  def one_click_purchases
+    customer = Stripe::Customer.retrieve(current_user.customer_id)
     charge = Stripe::Charge.create(
-      customer:     customer.id,   # You should store this customer id and re-use it.
+      customer:     customer.id,
       amount:       @order.amount_cents,
       description:  "Payment for order #{@order.id}",
       currency:     @order.amount.currency
     )
-
     @order.update(payment: charge.to_json, state: 'paid')
     redirect_to orders_complete_path(@order)
-
   rescue Stripe::CardError => e
     flash[:alert] = e.message
     redirect_to new_order_payment_path(@order)
