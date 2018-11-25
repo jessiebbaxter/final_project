adc4f8693934b0eecb9692dea636a9beb970f631
Jessie <jessie.baxter29@gmail.com>
2018-11-25 13:40:34 +1100

    Added inventory id to quick list item and updated logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,11 +13,21 @@ class PagesController < ApplicationController
  		@quick_buy_list = []
  		orders.each do |order|
  			order.inventories.each do |inventory|
- 				@quick_buy_list << {
- 					product: inventory.varient.product,
- 					inventory: inventory
- 				}
+ 				@quick_buy_list << QuickBuyItem.create(
+ 					user_id: current_user.id, 
+ 					product_id: inventory.varient.product.id,
+ 					inventory_id: inventory.id
+ 				)
  			end
  		end
  	end
+
+ 	def add_quick_buy
+    # quick_buy_item = QuickBuyItem.where(user_id: current_user)
+    # @products = []
+    # favorites.each do |fav|
+    #   @products << Product.find(fav.product_id)
+    # end
+    # @products.uniq!
+  end
 end
