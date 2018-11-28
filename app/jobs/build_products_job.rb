1789b402f0376c38d83d0512aafb529a9c7cadd0
Jessie <jessie.baxter29@gmail.com>
2018-11-28 18:06:40 +1100

    Seperated out mecca and target scraping jobs

--- app/jobs/build_products_job.rb
+++ app/jobs/build_products_job.rb
@@ -1,11 +1,13 @@
 class BuildProductsJob < ApplicationJob
   queue_as :default
 
-  def perform
+  def perform_target
     puts "Adding Target products"
     ScrapeTargetService.new.run
     puts "Finished adding Target products"
+  end
 
+  def perform_mecca
     puts "Adding Mecca products"
     ScrapeMeccaService.new.run
     puts "Finished adding Mecca products"
