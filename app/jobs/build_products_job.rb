f3833e28cc634fa600a6f06f8d3d792c4cd7c186
Jessie <jessie.baxter29@gmail.com>
2018-11-28 18:10:27 +1100

    Combined scrapes again into 'perform'

--- app/jobs/build_products_job.rb
+++ app/jobs/build_products_job.rb
@@ -1,15 +1,13 @@
 class BuildProductsJob < ApplicationJob
   queue_as :default
 
-  def perform_target
-    puts "Adding Target products"
-    ScrapeTargetService.new.run
-    puts "Finished adding Target products"
-  end
-
-  def perform_mecca
+  def perform
     puts "Adding Mecca products"
     ScrapeMeccaService.new.run
     puts "Finished adding Mecca products"
+
+    puts "Adding Target products"
+    ScrapeTargetService.new.run
+    puts "Finished adding Target products"
   end
 end
