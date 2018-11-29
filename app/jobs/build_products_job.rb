2b20390c57b04308d737372b7f8344198233ec43
Jessie <jessie.baxter29@gmail.com>
2018-11-29 12:43:04 +1100

    Updated Target scraping job

--- app/jobs/build_products_job.rb
+++ app/jobs/build_products_job.rb
@@ -2,10 +2,6 @@ class BuildProductsJob < ApplicationJob
   queue_as :default
 
   def perform
-    puts "Adding Mecca products"
-    ScrapeMeccaService.new.run
-    puts "Finished adding Mecca products"
-
     puts "Adding Target products"
     ScrapeTargetService.new.run
     puts "Finished adding Target products"
