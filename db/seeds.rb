089a11a96caf8529b647752331da35f766a80833
Jessie <jessie.baxter29@gmail.com>
2018-11-30 11:24:27 +1100

    Updated seed to randomly apply new inventory items to varients and randomly apply discounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,16 @@
-# puts 'Cleaning database...'
+puts "Creating 2 new Sellers"
 
-# puts 'Removing inventory...'
-# Inventory.destroy_all
-# puts 'Removed inventory'
+adore_beauty = Seller.create(domain: "Adore Beauty")
+look_fantastic = Seller.create(domain: "Look Fantastic")
 
-# puts 'Removing sellers...'
-# Seller.destroy_all
-# puts 'Removed sellers'
-
-# puts 'Removing variants...'
-# Varient.destroy_all
-# puts 'Removed variants'
-
-# puts 'Removing products...'
-# Product.destroy_all
-# puts 'Removed products'
-
-# puts 'Creating sellers...'
-# sellers = ["Target", "Sephora", "Mecca", "Cosmetics Now"]
-# sellers.each do |seller|
-# 	Seller.create(domain: seller)
-# 	puts "Created seller: #{seller}"
-# end
-
-# puts 'Starting Sephora scrape...'
-
-# ScrapeSephoraService.new.run(500, 7)
-
-# puts 'Finished Sephora scrape...'
-
-# if you stop the seed to save time from a long scrape,
-# comment out everything above this comment and run the seed
-# on just the below lines.
-
-puts 'Destroying shipping guidelines'
-Shipping.destroy_all
-puts 'Adding Shipping Guidelines...'
-
-target = Seller.find_by(domain: "Target")
-sephora = Seller.find_by(domain: "Sephora")
-mecca = Seller.find_by(domain: "Mecca")
-cosmetics_now = Seller.find_by(domain: "Cosmetics Now")
-
+puts "Assigning Shipping rules to Adore Beauty"
 
 Shipping.create!(
   minimum_spend: 0,
   shipping_speed: 5,
   rule: "2 - 5 business days",
   cost: 12,
-  seller_id: sephora.id
+  seller_id: adore_beauty.id
   )
 
 Shipping.create!(
@@ -56,33 +18,33 @@ Shipping.create!(
   shipping_speed: 8,
   rule: "3 - 8 business days",
   cost: 5,
-  seller_id: sephora.id
+  seller_id: adore_beauty.id
   )
 
 Shipping.create!(
-  minimum_spend: 55,
+  minimum_spend: 25,
   shipping_speed: 8,
   rule: "3 - 8 business days",
   cost: 0,
-  seller_id: sephora.id
+  seller_id: adore_beauty.id
   )
 
 Shipping.create!(
-  minimum_spend: 55,
+  minimum_spend: 25,
   shipping_speed: 5,
   rule: "2 - 5 business days",
   cost: 7,
-  seller_id: sephora.id
+  seller_id: adore_beauty.id
   )
 
-puts 'Finished Sephora'
+puts "Assigning Shipping rules to Look Fantastic"
 
 Shipping.create!(
   minimum_spend: 0,
   shipping_speed: 9,
   rule: "5 - 9 business days",
   cost: 9,
-  seller_id: target.id
+  seller_id: look_fantastic.id
   )
 
 Shipping.create!(
@@ -90,45 +52,48 @@ Shipping.create!(
   shipping_speed: 9,
   rule: "5 - 9 business days",
   cost: 0,
-  seller_id: target.id
+  seller_id: look_fantastic.id
   )
 
-puts 'Finished Target'
+puts "Assigning new inventory items"
 
-Shipping.create!(
-  minimum_spend: 0,
-  shipping_speed: 4,
-  rule: "2 - 4 business days",
-  cost: 10,
-  seller_id: mecca.id
-  )
+inventories = Inventory.all
 
-Shipping.create!(
-  minimum_spend: 75,
-  shipping_speed: 2,
-  rule: "1 - 2 business days",
-  cost: 0,
-  seller_id: mecca.id
-  )
-puts 'Finished Mecca'
+price_diff = [-300, -200, -100, 0, 100, 200, 300]
+sellers = Seller.all
 
-Shipping.create!(
-  minimum_spend: 0,
-  shipping_speed: 4,
-  rule: "2 - 4 business days",
-  cost: 0,
-  seller_id: cosmetics_now.id
-  )
+100.times do 
+  random_inventory = inventories.sample
+  seller_id = random_inventory.seller_id
+  remaining_sellers = sellers.reject { |seller| seller.id == seller_id }
+  price = random_inventory.price_cents
+  variant_id = random_inventory.varient_id
+  rand_source_url = (rand 30000...90000000).to_s
+  Inventory.create(source_url: rand_source_url, price_cents: (price + price_diff.sample), varient_id: variant_id, seller_id: remaining_sellers.sample.id)
+end
 
-Shipping.create!(
-  minimum_spend: 0,
-  shipping_speed: 2,
-  rule: "1 - 2 business days",
-  cost: 4,
-  seller_id: cosmetics_now.id
-  )
+puts "Finished assigning new inventory items"
 
-puts 'Finished Shipping'
+puts "Creating new coupons"
 
-puts 'Finished seed!'
+existing_coupon_seller_id = Coupon.last.seller_id
+remaining_sellers = sellers.reject { |seller| seller.id == existing_coupon_seller_id }
+
+rand_discount = [0.1, 0.15, 0.2]
+
+remaining_sellers.each do |seller|
+  Coupon.create(seller_id: seller.id, discount: rand_discount.sample)
+end
+
+puts "Applying coupons randomly to inventory"
+
+coupons = Coupon.all
+
+100.times do
+  inventory = inventories.sample
+  inventory.coupon_id = coupons.sample.id
+  inventory.save
+end
+
+puts "Finished seed!"
 
